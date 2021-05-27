bbe9902b086cda2cb8558081ad116770f26b251e
nithimy <57937879+nithimy@users.noreply.github.com>
2021-05-27 13:07:13 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-  baseURL: 'https://classi-ba5or.ondigitalocean.app/',
+  // baseURL: 'https://classi-ba5or.ondigitalocean.app/',
+  baseURL: 'https://classibackend.herokuapp.com/',
   // baseURL: 'http://localhost:3000/',
   timeout: 10000
 })
